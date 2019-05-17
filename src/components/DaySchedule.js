eea196bc699b4ac122e034484246ddab4494ca35
Evan He <hexianliang9@gmail.com>
2019-05-16 23:06:50 -0400

    default day template with no scrolling finished

--- src/components/DaySchedule.js
+++ src/components/DaySchedule.js
@@ -1,15 +1,47 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
-import Event from './Event.js';
+import { View } from 'react-native';
+import DefaultHourTemplate from './DefaultHourTemplate'
 
 class DaySchedule extends Component {
     render() {
+        const { topStyle } = styles;
         return (
             <View>
-                <Event/>
+                <View style={topStyle}/>
+                <DefaultHourTemplate hourText={'12 AM'}/>
+                <DefaultHourTemplate hourText={'1 AM'}/>
+                <DefaultHourTemplate hourText={'2 AM'}/>
+                <DefaultHourTemplate hourText={'3 AM'}/>
+                <DefaultHourTemplate hourText={'4 AM'}/>
+                <DefaultHourTemplate hourText={'5 AM'}/>
+                <DefaultHourTemplate hourText={'6 AM'}/>
+                <DefaultHourTemplate hourText={'7 AM'}/>
+                <DefaultHourTemplate hourText={'8 AM'}/>
+                <DefaultHourTemplate hourText={'9 AM'}/>
+                <DefaultHourTemplate hourText={'10 AM'}/>
+                <DefaultHourTemplate hourText={'11 AM'}/>
+                <DefaultHourTemplate hourText={'12 PM'}/>
+                <DefaultHourTemplate hourText={'1 PM'}/>
+                <DefaultHourTemplate hourText={'2 PM'}/>
+                <DefaultHourTemplate hourText={'3 PM'}/>
+                <DefaultHourTemplate hourText={'4 PM'}/>
+                <DefaultHourTemplate hourText={'5 PM'}/>
+                <DefaultHourTemplate hourText={'6 PM'}/>
+                <DefaultHourTemplate hourText={'7 PM'}/>
+                <DefaultHourTemplate hourText={'8 PM'}/>
+                <DefaultHourTemplate hourText={'9 PM'}/>
+                <DefaultHourTemplate hourText={'10 PM'}/>
+                <DefaultHourTemplate hourText={'11 PM'}/>
+                <DefaultHourTemplate hourText={'12 PM'}/>
             </View>);
     }
-};
+}
 
+const styles = {
+    topStyle: {
+        height: 10,
+        width: '100%'
+    }
+};
 
 export default DaySchedule;
---
+++ src/components/DefaultHourTemplate.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+class DefaultHourTemplate extends Component {
+    render() {
+        const { viewStyle, timeTextStyle } = styles;
+        return (
+            <View style={viewStyle}>
+                <View>
+                    <Text style={timeTextStyle}>{this.props.hourText}</Text>
+                </View>
+                <View
+                style={{
+                    borderTopColor: 'black',
+                    borderTopWidth: 1,
+                    width: '100%',
+                    height: 60,
+                    opacity: 0.3
+                }}
+                />
+            </View>);
+    }
+}
+
+const styles = {
+    viewStyle: {
+        flexDirection: 'row'
+    },
+    timeTextStyle:{
+        marginTop: -7,
+        width: 45
+    }
+};
+
+export default DefaultHourTemplate;
