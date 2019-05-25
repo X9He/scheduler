d3957aef4b72d11db6fa7a93dfbcc925cadc3c0f
Evan He <hexianliang9@gmail.com>
2019-05-25 02:05:57 -0400

    running router with nothing

--- src/Router.js
+++ src/Router.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Scene, Router } from 'react-native-router-flux/index';
+import { Scene, Router } from 'react-native-router-flux';
 import LoginPage from './components/LoginPage';
 import {Text, View, TextInput, Image, ScrollView, Switch} from 'react-native';
 import DatePicker from 'react-native-datepicker'
