6c00b0329a63ee26c4df21b950dd86e9e9613a88
Evan He <hexianliang9@gmail.com>
2019-05-25 01:33:42 -0400

    restructuring

---
+++ src/Router.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Scene, Router } from 'react-native-router-flux/index';
+import LoginPage from './components/LoginPage';
+import {Text, View, TextInput, Image, ScrollView, Switch} from 'react-native';
+import DatePicker from 'react-native-datepicker'
+
+
+class RouterComponent extends Component {
+
+    render () {
+        return (
+            <View/>
+        );
+    }
+};
+
+
+export default RouterComponent;
