64f162dadabe945d809818e6f2a27f7e2d3a21d3
Evan He <hexianliang9@gmail.com>
2019-05-29 00:13:33 -0400

    events can now be added to the day schedule and displayed

--- src/components/Event.js
+++ src/components/Event.js
@@ -2,13 +2,26 @@ import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 
 class Event extends Component {
+
     render() {
+        const { eventDefault, eventTextStyle } = styles;
         return (
-            <View>
-                <Text>Event</Text>
+            <View style={[this.props.eventStyle, eventDefault]}>
+                <Text style={eventTextStyle}>{this.props.title}</Text>
             </View>);
     }
 }
 
 
+const styles = {
+    eventDefault:{
+        borderRadius: 2,
+        opacity: 0.8
+    },
+    eventTextStyle: {
+        position:'absolute',
+        left: 8,
+        top: 8
+    }
+};
 export default Event;
