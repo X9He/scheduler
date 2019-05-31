48018a4fc17d3c70947c4be825fa9d01c41970c2
Tianjian Song <lucaslulusong@gmail.com>
2019-05-31 00:05:36 -0400

    search page with seach bar without any data (#12)

--- src/Router.js
+++ src/Router.js
@@ -5,6 +5,7 @@ import DaySchedule from "./components/DaySchedule";
 import AddEvent from "./components/AddEvent";
 import SignupPage from "./components/SignupPage";
 import RepeatPage from "./components/RepeatPage";
+import SearchPage from "./components/SearchPage";
 import AlertPage from "./components/AlertPage";
 
 
@@ -18,6 +19,7 @@ class RouterComponent extends Component {
                     <Scene key="DaySchedule" component={DaySchedule} initial/>
                     <Scene key="AddEvent" component={AddEvent}/>
                     <Scene key="SignupPage" component={SignupPage}/>
+                    <Scene key="SearchPage" component={SearchPage}/>
                     <Scene key="RepeatPage" component={RepeatPage}/>
                     <Scene key="AlertPage" component={AlertPage}/>
                 </Scene>
